4897f7702f939d13c4f31bdd255254262efa3db5
raul.kim <raul.kim@key-inside.com>
2021-11-10 18:37:32 +0900

    wrap address field 추가

--- token.go
+++ token.go
@@ -14,8 +14,9 @@ var token = map[string]string{
 	"initial_supply": "394100000000000000",
 	"fee":            "transfer=0.001,10000000000;pay=0.002",
 	// "target_address": "PCI01639F0B0A494B6040CE8B5B0DC4C56ACA7E78F6BAB02271AF", //PROD
-	"target_address": "{{PCI0186202E8A8EB235DC5FC1C69E03A9B0C567E97B0F05BEAD29}}", //local
-	"wpci_address":   "{{PCI0134B61CAE32413ABC249A5E5A3D13EF5B31C7EED2496DE881}}",
+	// "wpci_address":   "{{wrap_address}}", //PROD must check address
+	// "target_address": "PCI01CF0496510A7CD4890A0BF85F2ED0DFFF61C008A40EBAB45C", //local
+	// "wpci_address":   "PCI01AA090DB75243A3892E3422AD6EEC5AE314D8399275F325F2", //local
 	// unnecessary fields
 	"description": "Payprotocol",
 	"website":     "https://payprotocol.io/",
