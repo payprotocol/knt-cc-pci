ae70d5b68168c6d42a3e78f7c4958ec5f4cfb93a
raul.kim <raul.kim@key-inside.com>
2021-12-10 13:50:57 +0900

    fee 수정

--- token.go
+++ token.go
@@ -12,11 +12,11 @@ var token = map[string]string{
 	"decimal":        "8",
 	"max_supply":     "394100000000000000",
 	"initial_supply": "394100000000000000",
-	"fee":            "transfer=0.001,10000000000;pay=0.002",
+	"fee":            "transfer=0.001,10000000000;pay=0.002;wrap=0",
 	// "target_address": "PCI01639F0B0A494B6040CE8B5B0DC4C56ACA7E78F6BAB02271AF", //PROD
 	// "wpci_address":   "{{wrap_address}}", //PROD must check address
-	// "target_address": "PCI01CF0496510A7CD4890A0BF85F2ED0DFFF61C008A40EBAB45C", //local
-	// "wpci_address":   "PCI01AA090DB75243A3892E3422AD6EEC5AE314D8399275F325F2", //local
+	// "target_address": "PCI015DEE3F19CCDB7A652136A4B48B6BBD5758F30EAE7713DEB8", //local
+	// "wpci_address":   "PCI014CAAB7A72208A519FA4A75D915F5265AA00D3200E1FD5A54", //local
 	// unnecessary fields
 	"description": "Payprotocol",
 	"website":     "https://payprotocol.io/",
