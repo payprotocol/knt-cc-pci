e4868ff59e267a800d85713ccf9df7ee976df22d
Rooney Jung <rooney.jung@key-inside.com>
2022-02-22 13:10:11 +0900

    mainnet bridge address

--- token.go
+++ token.go
@@ -13,7 +13,8 @@ var token = map[string]interface{}{
 	"max_supply":     "394100000000000000",
 	"initial_supply": "394100000000000000",
 	"fee":            "transfer=0.001,10000000000;pay=0.002",
-	"target_address": "PCI01639F0B0A494B6040CE8B5B0DC4C56ACA7E78F6BAB02271AF",
+	// "target_address": ""	// testnet (genesis)
+	"target_address": "PCI01639F0B0A494B6040CE8B5B0DC4C56ACA7E78F6BAB02271AF", // mainnet
 	// unnecessary fields
 	"description": "Payprotocol",
 	"website":     "https://payprotocol.io/",
@@ -24,7 +25,8 @@ var token = map[string]interface{}{
 var wrapBridge = map[string]string{
 	// "{{ext_code}}":"{{wrap_address}};{{ext_chain}}"
 	// eg. "wpci": "PCI0119043794E2DCB197059D2304E6AABB8501B32B8C6F116CB0;ETH"
-	"wpci": "PCI01B8C378A079BD434B7B7AE383BB4D43FFDE7FFBE717B03637;ETH",
+	// "wpci": "PCI01B6AC0313B49F37934225D53720E71C2B63872C9AC2BAAABF;ETH",	// testnet
+	"wpci": "PCI0147A0FFEA1F2A1FA5D08967D297D29CA495CEC34E5BDF11F3;ETH", // mainnet
 }
 
 func mintableAmount(totalSupply *big.Int, balance *big.Int, amount *big.Int) (*big.Int, error) {
