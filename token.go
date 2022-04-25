59e59dc4150eeb2b957f2311eb243ccc90bdbbd8
payprotocol <43838367+payprotocol@users.noreply.github.com>
2022-04-25 15:21:22 +0900

    Update token.go
    update testnet address

--- token.go
+++ token.go
@@ -13,7 +13,7 @@ var token = map[string]interface{}{
 	"max_supply":     "394100000000000000",
 	"initial_supply": "394100000000000000",
 	"fee":            "transfer=0.001,10000000000;pay=0.002", // 0.001 = 0.1%, 10000000000 = 100PCI, 0.002 = 0.2%
-	// "target_address": ""	// testnet (genesis)
+	// "target_address": "PCI010A7733130C356643AE671FFA962E9D34AD9A595EF6294D5E" // testnet
 	"target_address": "PCI01639F0B0A494B6040CE8B5B0DC4C56ACA7E78F6BAB02271AF", // mainnet
 	"wrap_bridge":    wrapBridge,
 	// optional fields
@@ -23,8 +23,8 @@ var token = map[string]interface{}{
 
 var wrapBridge = map[string]string{
 	// "{{ext_code}}":"{{wrap_address}};{{ext_chain}}"
-	// eg. "wpci": "PCI0119043794E2DCB197059D2304E6AABB8501B32B8C6F116CB0;ETH"
-	// "wpci": "PCI01B6AC0313B49F37934225D53720E71C2B63872C9AC2BAAABF;ETH",	// testnet
+	// eg. "wpci": "PCI01AE18973BDC22C8834620E86B2EFF77988F9D340D840409D0;ETH"
+	// "wpci": "PCI01AE18973BDC22C8834620E86B2EFF77988F9D340D840409D0;ETH",	// testnet
 	"wpci": "PCI0147A0FFEA1F2A1FA5D08967D297D29CA495CEC34E5BDF11F3;ETH", // mainnet
 }
 
